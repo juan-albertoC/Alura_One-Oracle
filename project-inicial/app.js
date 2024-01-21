f21c5733d5bf71883b259f404242094fc2601c79
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-01-20 20:39:58 -0600

    Comparacion if else con Template strings una interpolacion

--- project-inicial/app.js
+++ project-inicial/app.js
@@ -8,3 +8,13 @@ let numeroUsuario = prompt("Me indicas un numero entre 1 y 10, por favor: ");
 //consola
 console.log(numeroUsuario);
 
+/*
+Realiza la comparacion if and else
+*/
+if(numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadero, se usa: Template strings una interpolacion
+    alert(`Acertaste, el numero es: ${numeroUsuario}`);
+} else {
+    //No Acertamos, la condicion es falsa
+    alert("No Acertaste al numero");
+}
