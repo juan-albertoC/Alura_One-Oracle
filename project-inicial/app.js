db180cc2115f29e74cd88845741c101d2129f24b
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-01-20 19:55:13 -0600

    Asignacion y declaracion variables en prompt, llamada de consola y alert

---
+++ project-inicial/app.js
@@ -0,0 +1,10 @@
+//mensaje de bienvenida
+alert("Bienvenidos al juego del numero secreto");
+
+//variables
+let numeroSecreto = 7;
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10, por favor: ");
+
+//consola
+console.log(numeroUsuario);
+
