8880e61a690a614d2fd3121ac863617bf7660185
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-01-22 04:55:24 -0600

    Generar valor pseudo-aleatorio con funcion math random floor, probar funcionamiento console log Verificar en console.log el tipo de dato con typeof,ademas incluir una conversion al entero con parseInt y que se almacena en variable

--- project-inicial/app.js
+++ project-inicial/app.js
@@ -1,8 +1,8 @@
 //mensaje de bienvenida
 alert("Bienvenidos al juego del numero secreto");
 
-//variable declarada y asignada
-let numeroSecreto = 7;
+//variable declarada y asignada, cambiar el valor pseudo-aleatorio random y floor con funcion math
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 
 //variable valor inicial, se salio esta variable del while
 let numeroUsuario = 0;
@@ -16,12 +16,16 @@ let intentos = 1;
 //variable para maximo de intentos
 let maximosIntentos = 3;
 
+//console.log para verificar el funcionamiento de pseudo-aleatorio
+//console.log(numeroSecreto);
+
 //while persona no acierte compara para repitir la accion de jugar hasta ganar
 while (numeroUsuario != numeroSecreto) {
-     numeroUsuario = prompt("Me indicas un numero entre 1 y 10, por favor: ");
+    //parseInt(); para una conversion al entero, almacena en variable 
+     numeroUsuario = parseInt(prompt("Me indicas un numero entre 1 y 10, por favor: "));
 
     //consola
-    console.log(numeroUsuario);
+    console.log(typeof(numeroUsuario));
 
     /*
     Realiza la comparacion if and else
