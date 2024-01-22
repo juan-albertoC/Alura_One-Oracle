0c2e5d5ce8879c870d31736135e1ace7ff655510
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-01-21 23:06:50 -0600

    Create if else anidado, while para comparar, variable valor inicial e identacion

--- project-inicial/app.js
+++ project-inicial/app.js
@@ -1,20 +1,32 @@
 //mensaje de bienvenida
 alert("Bienvenidos al juego del numero secreto");
 
-//variables
+//variable declarada y asignada
 let numeroSecreto = 7;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10, por favor: ");
 
-//consola
-console.log(numeroUsuario);
+//variable valor inicial, se salio esta variable del while
+let numeroUsuario = 0;
 
-/*
-Realiza la comparacion if and else
-*/
-if(numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadero, se usa: Template strings una interpolacion
-    alert(`Acertaste, el numero es: ${numeroUsuario}`);
-} else {
-    //No Acertamos, la condicion es falsa
-    alert("No Acertaste al numero");
+//while persona no acierte compara para repitir la accion de jugar hasta ganar
+while (numeroUsuario != numeroSecreto) {
+     numeroUsuario = prompt("Me indicas un numero entre 1 y 10, por favor: ");
+
+    //consola
+    console.log(numeroUsuario);
+
+    /*
+    Realiza la comparacion if and else
+    */
+    if(numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadero, se usa: Template strings una interpolacion
+        alert(`Acertaste, el numero es: ${numeroUsuario}`);
+    } else {
+        //No Acertamos, es falso, con condicion if  else anidado
+        if (numeroUsuario > numeroSecreto){
+            alert("El numero secreto es menor");
+        } else {
+            alert("El numero secreto es mayor");
+        }
+
+    }
 }
