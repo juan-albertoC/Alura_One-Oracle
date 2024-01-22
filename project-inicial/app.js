996588132b0eb5c3d6e1bc53e605ae55c4908e12
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-01-22 00:09:24 -0600

    Create variable contador de intentos asignado en 1 e incrementar contador explicito cuando no acierta Create variable string vez, update tras incremento Agregar variables a interpolacion template strings

--- project-inicial/app.js
+++ project-inicial/app.js
@@ -7,6 +7,13 @@ let numeroSecreto = 7;
 //variable valor inicial, se salio esta variable del while
 let numeroUsuario = 0;
 
+//variable contador de intentos, 1 porque si acierta se usa ese intento
+let intentos = 1;
+
+//variable tipo string para el contador que varia la linguistica de vez o veces que acierta el usuario
+let palabraVeces = 'vez';
+
+
 //while persona no acierte compara para repitir la accion de jugar hasta ganar
 while (numeroUsuario != numeroSecreto) {
      numeroUsuario = prompt("Me indicas un numero entre 1 y 10, por favor: ");
@@ -19,7 +26,7 @@ while (numeroUsuario != numeroSecreto) {
     */
     if(numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadero, se usa: Template strings una interpolacion
-        alert(`Acertaste, el numero es: ${numeroUsuario}`);
+        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
     } else {
         //No Acertamos, es falso, con condicion if  else anidado
         if (numeroUsuario > numeroSecreto){
@@ -27,6 +34,9 @@ while (numeroUsuario != numeroSecreto) {
         } else {
             alert("El numero secreto es mayor");
         }
-
+        //Incrementar contador explicito cuando persona no acierta
+        intentos = intentos + 1;
+        //Actualizar tras el incremento del contador para variable palabraVeces
+        palabraVeces = 'veces';
     }
 }
