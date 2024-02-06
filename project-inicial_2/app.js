1bc61fdb2e3bacbfea5fdfe565696568e98f3650
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-02-05 19:38:04 -0600

    Add source of script app.js into index.html Add etiqueta h1 and p en index Trabajar con el DOM en app.js con el metodo querySelector asi cambie el titulo de las etiquetas h1 and p Llamar la funcion con el evento onclick para los botones Agregar en app.js la funcion intentoDeUsuario y una accion para el boton

---
+++ project-inicial_2/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero Secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica Numero del 1 al 10';
+
+function intentoDeUsuario(){
+
+    alert('Click desde el boton');
+}
