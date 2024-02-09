25ff0b176dc5920000da4bc64793d8c54b4f3783
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-02-09 00:54:57 -0600

    funcion que genera y retorna un numero aleatorio asignacion de la funcion a la variable de ambito global y asi verificar su funcionamiento console

--- project-inicial_2/app.js
+++ project-inicial_2/app.js
@@ -3,22 +3,40 @@
    08 febrero 2024   
    fundamentos de programacion 
 */
-
+//--------------------------------------------------------------
 //metodo querySelector con parametro etiqueta p
 //let parrafo = document.querySelector('p');
 //agrega o inserta texto en variable parrafo
 //parrafo.innerHTML = 'Indica Numero del 1 al 10';
+//--------------------------------------------------------------
+
+//asignacion de la funcion a la variable de ambito global
+let numeroSecreto = generarNumeroSecreto();
+
+//verificar su funcionamiento
+console.log(numeroSecreto);
 
 //encapsulamiento
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;   //No retorna nada
 }
 
 
 function intentoDeUsuario(){
     //mensaje de alert tras hacer click en el boton
     alert('Click desde el boton');
+    return;   //No retorna nada, colocarlo por buena practica 
+}
+
+//funcion que genera un numero aleatorio
+function generarNumeroSecreto(){
+    //let numeroSecreto = Math.floor(Math.random()*10)+1;
+    //en vez de tener una variable interna de bloque, simple con un return
+    return Math.floor(Math.random()*10)+1; 
+    //retorno anterior del numeroSecreto
+     
 }
 
 //llamar e invocar la funcion y paso de valor de los parametros
