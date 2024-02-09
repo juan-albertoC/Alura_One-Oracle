c8d1e8e41af7fd19714f682e6cbed85b254a2c9b
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-02-09 00:07:13 -0600

    create funciones genericas con parametros para ser reutilizadas,uso de encapsulamiento, asi mismo llamar e invocar la funcion con paso de valor de tales parametros

--- project-inicial_2/app.js
+++ project-inicial_2/app.js
@@ -1,10 +1,27 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero Secreto';
+/*
+   juan alberto cuevas juarez 
+   08 febrero 2024   
+   fundamentos de programacion 
+*/
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica Numero del 1 al 10';
+//metodo querySelector con parametro etiqueta p
+//let parrafo = document.querySelector('p');
+//agrega o inserta texto en variable parrafo
+//parrafo.innerHTML = 'Indica Numero del 1 al 10';
+
+//encapsulamiento
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
 
 function intentoDeUsuario(){
-
+    //mensaje de alert tras hacer click en el boton
     alert('Click desde el boton');
 }
+
+//llamar e invocar la funcion y paso de valor de los parametros
+asignarTextoElemento('h1','Juego del numero secreto!');
+
+asignarTextoElemento('p','Indica un numero del 1 al 10, por favor');
