782edcea2bad79a542d5f7bccd2431d061052dc1
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-02-23 01:10:45 -0600

    create files and declaracion,asignacion de variables asi como alert, prompt, if

---
+++ problemasLogica/desafio-1/app.js
@@ -0,0 +1,32 @@
+/*
+Juan Alberto Cuevas Juarez
+22 Febrero 2024
+practica de problemas de logica y ejercicios
+
+*/
+//mensaje de bienvenida
+alert("Hello I am Juan Cuevas");
+//alert("¡Bievenido a nuestro sitio web!");
+
+//declarar y asignar valor a variables
+let nombre = "Luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+//muestra alerta con el texto
+alert("¡Error! Completa todos los campos");
+
+//declaracion y asignacion a variable
+let mensajeDeError = "¡Error! Completa todos los campos";
+//muestra una alerta con el valor de la variable
+alert(mensajeDeError);
+
+//utilizar prompt preguntar al usuario su nombre y almacenar en variable dicho valor
+let nombre = prompt("¿ Nombre de usuario: ");
+let edad = prompt("¿ Edad de usuario: ");
+
+//condicional if
+if (edad >= 18){
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
