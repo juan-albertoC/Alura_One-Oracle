bcea57a5d8fd012ac0e68e07eeba139693bb9811
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-03-01 01:07:07 -0600

    resolver problema de recursividad con el uso de una condicion de salida como validador con un mensaje o seguir jugando

--- project-inicial_4/app.js
+++ project-inicial_4/app.js
@@ -2,6 +2,9 @@ let numeroSecreto = 0;
 
 let intentos = 0;
 
+//para cambiar aquellos valores estaticos
+let numeroMaximo = 10;
+
 //declarar lista de numeros sorteados
 let listaNumerosSorteados = [];
 
@@ -51,25 +54,33 @@ function limpiarCaja(){
 function generarNumeroSecreto(){
     //return Math.floor(Math.random()*10)+1;
     
-     let numeroGenerado = Math.floor(Math.random()*10)+1;
+     let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
       
      //console.log para la lista y numero generado
      console.log(numeroGenerado);
      console.log(listaNumerosSorteados);
-     
-     //si numero generado esta en la lista es decir si el numero generado esta incluido en la lista
-     if (listaNumerosSorteados.includes(numeroGenerado)){
-        //elemento ya existe en la lista recursividad llamar misma funcion
-        return generarNumeroSecreto(); //reutilizar la funcionalidad
-        
+
+     //si ya sorteamos todos los numeros
+     if(listaNumerosSorteados.length == numeroMaximo){
+         
+          //texto
+          asignarTextoElemento('p', 'ya se sorteron todos los numeros posibles');
 
      } else {
-        //elemnto no existe manda el nuevo elemento al final con el metodo push
-        listaNumerosSorteados.push(numeroGenerado);
-        
+        //si numero generado esta en la lista es decir si el numero generado esta incluido en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)){
+            //elemento ya existe en la lista recursividad llamar misma funcion
+            return generarNumeroSecreto(); //reutilizar la funcionalidad
+            
 
-        return numeroGenerado;
-     }
+        } else {
+            //elemnto no existe manda el nuevo elemento al final con el metodo push
+            listaNumerosSorteados.push(numeroGenerado);
+            
+
+            return numeroGenerado;
+        }
+    }
 }
 
 
@@ -77,7 +88,8 @@ function condicionesIniciales(){
 
 asignarTextoElemento('h1','Juego del numero secreto!');
 
-asignarTextoElemento('p','Indica un numero del 1 al 10, por favor');
+//template strings
+asignarTextoElemento('p',`Indica un numero del 1 al ${numeroMaximo}`);
 
    numeroSecreto = generarNumeroSecreto();
    
